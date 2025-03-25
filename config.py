7e265660dd4f3bb0ac2c1378d47da99bad99b563
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2025-03-25 13:05:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,32 @@
 import os
 
 class Config(object):
-    # Telegram Bot ka token
-    BOT_TOKEN = "7950961856:AAEGWvV30I27JytUYFxKJ63iqI3XtmonB34"
-    # Telegram API ki ID
-    API_ID = 27030874
-    # Telegram API ki hash key
-    API_HASH = "da842f6a91019edf0ac74b6e00c43c91"
-    ADMIN_ID = [7991495621]
-    # MongoDB database ka URL
-    DB_URL = "mongodb+srv://tanujaXmusic:tanujaXmusic@tanujaXmusic.octnw1p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    # Database ka naam
+    
+    BOT_TOKEN = "7903490948:AAFxqepZ4V0sG3hhr45Xvv55jQj2oJOg8vQ"
+    
+    API_ID = 28094744
+    
+    API_HASH = "a75af4285edc7747c57bb19147ca0b9b"
+    ADMIN_ID = [28094744]
+    
+    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    
     DB_NAME = "MY_BOT_DB"
-    # Text log channel ki ID
-    TXT_LOG = -10012234502065
-    # Authentication log channel ki ID
-    AUTH_LOG = -1002429764735
-    # Hit log channel ki ID
-    HIT_LOG = -1002262405901
-    # DRM dump channel ki ID
-    DRM_DUMP = -1002349629748
+    
+    TXT_LOG = -1002416535442
+    
+    AUTH_LOG = -1002416535442
+    
+    HIT_LOG = -1002416535442
+    
+    DRM_DUMP = -1002416535442
     # Main channel ki ID
-    CHANNEL = -1002361507681
+    CHANNEL = -1002416535442
     # Channel ka link
-    CH_URL = "https://t.me/+qEEho66dZJ1kODRi"
-    # Bot ke owner ka Telegram link
-    OWNER = "https://t.me/skywardeeagle"
-    # Thumbnail image ka URL
-    THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
-    # API host ka URL
+    CH_URL = "https://t.me/nsmmsksmsmsmsmm"
+    
+    OWNER = "https://t.me/Nikhil_saini_khe"
+    
+    THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
+   
     HOST = "https://api.masterapi.tech"
