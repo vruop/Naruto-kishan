3fdfc54c3e754c4890b593d7107bc9925b4b306d
Nitinyadav798900 <nitinyadav798900@gmail.com>
2025-03-31 14:33:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,30 +2,30 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = "7903490948:AAFxqepZ4V0sG3hhr45Xvv55jQj2oJOg8vQ"
+    BOT_TOKEN = "8012231272:AAFITViiSvq-2hXqbCaS4VBwHahNsim-woE"
     
-    API_ID = 28094744
+    API_ID = 12475131
     
-    API_HASH = "a75af4285edc7747c57bb19147ca0b9b"
-    ADMIN_ID = [28094744]
+    API_HASH = "719171e38be5a1f500613837b79c536f"
+    ADMIN_ID = [12475131]
     
     DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     
     DB_NAME = "MY_BOT_DB"
     
-    TXT_LOG = -1002416535442
+    TXT_LOG = -1002542634912
     
-    AUTH_LOG = -1002416535442
+    AUTH_LOG = -1002542634912
     
-    HIT_LOG = -1002416535442
+    HIT_LOG = -1002542634912
     
-    DRM_DUMP = -1002416535442
+    DRM_DUMP = -1002542634912
     # Main channel ki ID
-    CHANNEL = -1002416535442
+    CHANNEL = -1002542634912
     # Channel ka link
-    CH_URL = "https://t.me/nsmmsksmsmsmsmm"
+    CH_URL = "https://t.me/gjskisb"
     
-    OWNER = "https://t.me/Nikhil_saini_khe"
+    OWNER = "https://t.me/gjskisb"
     
     THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
    
