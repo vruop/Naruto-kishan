b3049abe7559ccad5782a677243d01f453c087fd
vruop <q803816@gmail.com>
2025-06-02 13:27:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,30 +2,30 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = "8012231272:AAFITViiSvq-2hXqbCaS4VBwHahNsim-woE"
+    BOT_TOKEN = "7285235045:AAHr2u01d4zEY_vimFa1PUfXJReJJK_GJ20"
     
-    API_ID = 12475131
+    API_ID = 25566754
     
-    API_HASH = "719171e38be5a1f500613837b79c536f"
-    ADMIN_ID = [12475131]
+    API_HASH = "27609daf8ee847978797a359024e7607"
+    ADMIN_ID = [2073438175]
     
     DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     
-    DB_NAME = "MY_BOT_DB"
+    DB_NAME = "MY_XOROBOT_DB"
     
-    TXT_LOG = -1002542634912
+    TXT_LOG = -1002684037729
     
-    AUTH_LOG = -1002542634912
+    AUTH_LOG = -1002684037729
     
-    HIT_LOG = -1002542634912
+    HIT_LOG = -1002684037729
     
-    DRM_DUMP = -1002542634912
+    DRM_DUMP = -1002684037729
     # Main channel ki ID
-    CHANNEL = -1002542634912
+    CHANNEL = -1002684037729
     # Channel ka link
-    CH_URL = "https://t.me/gjskisb"
+    CH_URL = "https://t.me/+222qgZyImMw1MzY9"
     
-    OWNER = "https://t.me/gjskisb"
+    OWNER = "https://t.me/+222qgZyImMw1MzY9"
     
     THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
    
